c70677837daa20af459f9d35079dbf370d86ee53
Anderson <anderson1233211234567@gmail.com>
2024-05-27 18:29:11 +0800

    列出比特幣搜尋網頁的標題

--- tutorial.py
+++ tutorial.py
@@ -16,16 +16,19 @@ driver = webdriver.Edge(options=option,service=s)
 
 driver.get("HTTPS://google.com")
 
-#titles = driver.find_elements_by_class_name("board-nuser") 沒有這個方法
-
 search = driver.find_element(By.CLASS_NAME, "gLFyf")
 search.send_keys("比特幣")
 search.send_keys(Keys.RETURN)
+time.sleep(2)
+
+titles = driver.find_elements(By.XPATH, "//h3")
+for title in titles:
+    print(title.text)
 
 #search = driver.find_element(By.NAME, "query")
 #search.send_keys("比特幣")
 #search.send_keys(Keys.RETURN) #會被Dcard擋
 
 
-#time.sleep(5)
-#driver.quit()
+time.sleep(5)
+driver.quit()
