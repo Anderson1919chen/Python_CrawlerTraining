2077b923ac6cf114a66a2f41cd4bf6f4691b35dc
Anderson <anderson1233211234567@gmail.com>
2024-05-29 15:43:06 +0800

    ptt標題抓取

--- tutorial.py
+++ tutorial.py
@@ -1,37 +1,45 @@
 from selenium import webdriver 
 from selenium.webdriver.edge.service import Service
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 import time
 import requests
 
-
-
 option = webdriver.EdgeOptions()
 option.add_experimental_option("detach", True)
-
-s = Service(executable_path=r"C:/Users/User/Desktop/selenium_tutorial/msedgedriver.exe")
-
+s = Service(executable_path=r"C:/Users/User/Desktop/edgedriver1/msedgedriver.exe")
 driver = webdriver.Edge(options=option,service=s)
 
-driver.get("HTTPS://google.com")
+# google搜尋比特幣
+#driver.get("HTTPS://google.com")
+#search = driver.find_element(By.CLASS_NAME, "gLFyf")
+#search.clear()
+#search.send_keys("比特幣")
+#search.send_keys(Keys.RETURN) #按下鍵盤上的ENTER
+#time.sleep(2)
+#titles = driver.find_elements(By.XPATH, "//h3")
+#for title in titles:
+#    print(title.text)# 取得標籤內部的文字
 
-search = driver.find_element(By.CLASS_NAME, "gLFyf")
-search.send_keys("比特幣")
-search.send_keys(Keys.RETURN)
-time.sleep(2)
+# ptt
+driver.get("https://www.ptt.cc/bbs/index.html")
+link = driver.find_element(By.CLASS_NAME, "board-name")
+link.click()
+button = driver.find_element(By.CLASS_NAME, "btn-big")
+button.click()
 
-titles = driver.find_elements(By.XPATH, "//h3")
-for title in titles:
-    print(title.text)# 取得標籤內部的文字
+WebDriverWait(driver, 10).until(
+    EC.presence_of_element_located((By.CLASS_NAME, "board"))#等到頁面上的class=board出現才會繼續執行
+)
 
-driver.get("https://www.dcard.tw/f")
-search = driver.find_element(By.NAME, "query")
-time.sleep(2)
-search.send_keys("比特幣")
-time.sleep(2)
-search.send_keys(Keys.RETURN) #會被Dcard擋
+titles_ptt = driver.find_elements(By.CLASS_NAME, "title")
+for title_ptt in titles_ptt:
+    print(title_ptt.text)
 
+page = driver.find_element(By.LINK_TEXT, "‹ 上頁")
+page.click()
 
 time.sleep(5)
 driver.quit()
